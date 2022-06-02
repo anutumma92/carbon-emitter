0ab82a3fe0e5cfdf38f0575acc605cc4d8fccd71
heliofigueira <helio.figueira@instafreight.com>
2022-06-02 15:51:16 +0200

    Returning multiple responses

--- app/Services/HereMapsService.php
+++ app/Services/HereMapsService.php
@@ -12,13 +12,28 @@ class HereMapsService
 
     public string $app_code;
 
+    public array $alternativesParameters = [
+        [
+            'mode' => 'fastest;truck',
+            'waypoint0' => 'geo!50.8857,14.81589',
+            'waypoint1' => 'geo!50.8681536,14.8308207',
+            'metricSystem' => 'metric',
+            'maneuverAttributes' => 'shape',
+        ],
+        [
+            'mode' => 'fastest;truck',
+            'waypoint0' => 'geo!50.8857,14.81589',
+            'waypoint1' => 'geo!50.8681536,14.8308207',
+            'metricSystem' => 'metric',
+            'maneuverAttributes' => 'shape',
+        ]
+    ];
 
     public function __construct()
     {
         $this->url = env('HEREMAPS_URL');
         $this->app_id = env('HEREMAPS_ID');
         $this->app_code= env('HEREMAPS_CODE');
-
     }
 
     /**
@@ -26,30 +41,48 @@ class HereMapsService
      */
     public function calculateRoute()
     {
-        $routeInformation = $this->getRouteQueryString();
+        $routeAlternatives = $this->getRouteQueryString();
+        $responsePerAlternatives = [];
 
-        $client = new Client();
-        $res = $client->request('GET', $this->url, [
-            'query' => $routeInformation
-        ]);
+        foreach ($routeAlternatives as $index => $route)
+        {
+            $client = new Client();
+            $res = $client->request('GET', $this->url, [
+                'query' => $route
+            ]);
 
-        return json_decode($res->getBody()->getContents());
+            $responseData = json_decode($res->getBody()->getContents(),true);
+            $responseData = $this->filterResponse($responseData);
+
+            $responsePerAlternatives[$index] = $responseData;
+        }
+
+        return $responsePerAlternatives;
+    }
+
+    protected function filterResponse(array $response) : array
+    {
+        return [
+            'total_distance' => $response['response']['route'][0]['summary']['distance'],
+            'travel_time' => $response['response']['route'][0]['summary']['travelTime'],
+            'travel_text' => $response['response']['route'][0]['summary']['text'],
+        ];
     }
 
     protected function getRouteQueryString() : array
     {
-        $extraFilters = [
-            'mode' => 'fastest;truck',
-            'waypoint0' => 'geo!50.8857,14.81589',
-            'waypoint1' => 'geo!50.8681536,14.8308207',
-            'metricSystem' => 'metric',
-            'maneuverAttributes' => 'shape',
-        ];
+        $extraFilters = $this->alternativesParameters;
 
-        return [
-            'app_id' => $this->app_id,
-            'app_code' => $this->app_code
-        ] + $extraFilters;
+        $alternativeParameters = [];
+
+        foreach ($extraFilters as $index => $filter){
+            $alternativeParameters[$index] = [
+                'app_id' => $this->app_id,
+                'app_code' => $this->app_code
+            ] + $filter;
+        }
+
+        return $alternativeParameters;
     }
 
 }
